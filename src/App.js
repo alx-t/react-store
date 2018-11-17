044d669b82e751a704407618cfb184e4acd64196
Alex Tesevich <alex.tesevich@gmail.com>
2018-11-17 12:46:35 +0300

    Поправил замечания

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 import ShoppingCart from '~/src/components/containers/ShoppingCart';
+import CatalogPage from '~/src/components/CatalogPage';
 
 export default class App extends Component {
   render() {
     return (
-      <ShoppingCart />
+      <ShoppingCart>
+        <CatalogPage />
+      </ShoppingCart>
     );
   }
 }
--- src/components/containers/ShoppingCart.js
+++ src/components/containers/ShoppingCart.js
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types';
 
 import cartContext from '~/src/cartContext';
 
-import LineItem from './LineItem';
-import CatalogPage from '../CatalogPage';
+class LineItem {
+  constructor(id, title, quantity, price) {
+    this._id = id;
+    this._title = title;
+    this._quantity = quantity;
+    this._price = price;
+    this._total = this._price * this._quantity;
+  }
+
+  get total() { return this._total; }
+  get id() { return this._id; }
+  get title() { return this._title; }
+  get quantity() { return this._quantity; }
+  get price() { return this._price; }
+}
 
 class ShoppingCart extends Component {
   constructor(props) {
@@ -18,23 +31,21 @@ class ShoppingCart extends Component {
 
   addToCart(id, title, quantity, price) {
     const li = new LineItem(id, title, quantity, price);
-    const newItems = this.state.items;
-    newItems.push(li);
+    const newItems = this.state.items.concat(li);
     this.setState((prevState) => ({ items: newItems, total: prevState.total + li.total }));
   }
 
   render() {
-    const catalogPage = this.props.children;
     return (
       <cartContext.Provider value={{ cart: this.state, addToCart: this.addToCart }}>
-        <CatalogPage />
+        {this.props.children}
       </cartContext.Provider>
     );
   }
 }
 
 ShoppingCart.propTypes = {
-  children: PropTypes.instanceOf(CatalogPage)
+  children: PropTypes.instanceOf(Component)
 };
 
 export default ShoppingCart;
