bdee659d3de0b21c564302923bd27a0b40e18111
Alex Tesevich <alex.tesevich@gmail.com>
2018-12-26 15:27:30 +0300

    Статическое приложение для деплоя

--- initializers/webpack/common.js
+++ initializers/webpack/common.js
@@ -19,13 +19,6 @@ module.exports = {
         use: 'babel-loader'
       },
       {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader', options: { sourceMap: true } },
-          { loader: 'css-loader', options: { sourceMap: true } },
-        ]
-      },
-      {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         loader: 'url-loader?limit=100000'
       }
--- initializers/webpack/production.js
+++ initializers/webpack/production.js
@@ -1,9 +1,12 @@
+// const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
 const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./common');
 
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -16,6 +19,18 @@ module.exports = merge(common, {
     filename: 'assets/[name].[hash].js'
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  },
+
   plugins: [
     new webpack.DefinePlugin({
       NODE_ENV: 'production'
@@ -23,6 +38,12 @@ module.exports = merge(common, {
     new htmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[hash].css'
     })
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static'
+    // })
   ]
 });
